407aa274bc301970f2baad4c2dae6bd7275d9e62
Jeferson Tramonti <jefersontramonti@outlook.com>
2021-11-10 22:17:41 -0300

    H2 database OK

---
+++ backend/src/main/java/com/jt/backend/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.jt.backend.services;
+
+import com.jt.backend.entities.Category;
+import com.jt.backend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+}
