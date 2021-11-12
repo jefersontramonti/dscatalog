63155af1f7619cc6454064fa09c069e0b5bb5948
Jeferson Tramonti <jefersontramonti@outlook.com>
2021-11-12 20:45:10 -0300

    database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) values ( 'Books' );
+INSERT INTO tb_category (name) values ( 'Casa' );
+INSERT INTO tb_category (name) values ( 'Amor' );
+INSERT INTO tb_category (name) values ( 'Vida' );
